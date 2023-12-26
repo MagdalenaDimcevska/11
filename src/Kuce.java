554e8d5d7cf3d9f0a42ca41b155eb1ebdb04fa63
MagdalenaDimcevska <147504217+MagdalenaDimcevska@users.noreply.github.com>
2023-12-26 13:40:32 +0100

    Add first java project

---
+++ src/Kuce.java
@@ -0,0 +1,9 @@
+public class Kuce {
+public void osobina() {
+	System.out.println("Kuceto lae");
+}
+}
+class LoveckoKuce extends Kuce {
+	public void osobina() {
+	System.out.println("Loveckoto kuce duska");
+}}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+	public static void main(String[] args) {
+		Kuce o=new Kuce();
+		o.osobina();
+		LoveckoKuce o1=new LoveckoKuce();
+		o1.osobina();
+	}
+}
